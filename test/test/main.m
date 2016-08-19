575d19af3e2885646bccba393b7bcf6ccc980a27
azhang <13396707354@163.com>
2016-08-19 16:47:47 +0800

    初始化项目

---
+++ test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by azhang on 16/8/19.
+//  Copyright © 2016年 azhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
